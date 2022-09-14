1ad1b7e13bfd79270c6897f61f0b3fe35385530b
satriatama <satriatama121@gmail.com>
2022-09-14 12:34:10 +0700

    Initial commit

---
+++ app/src/main/java/com/satriatama/belajar/android/praktikum/MainActivity.java
@@ -0,0 +1,14 @@
+package com.satriatama.belajar.android.praktikum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
